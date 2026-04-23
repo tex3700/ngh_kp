c62cad567b6c1aa5ccad7f5735eee032fdbc4218
Dmitry <tex3700@gmail.com>
2026-04-23 20:49:16 +0300

    Laravel deploy

---
+++ app/MoonShine/Layouts/MoonShineLayout.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\MoonShine\Layouts;
+
+use MoonShine\Laravel\Layouts\AppLayout;
+use MoonShine\ColorManager\Palettes\SkyPalette;
+use MoonShine\ColorManager\ColorManager;
+use MoonShine\Contracts\ColorManager\ColorManagerContract;
+use MoonShine\Contracts\ColorManager\PaletteContract;
+
+final class MoonShineLayout extends AppLayout
+{
+    /**
+     * @var null|class-string<PaletteContract>
+     */
+    protected ?string $palette = SkyPalette::class;
+
+    protected function assets(): array
+    {
+        return [
+            ...parent::assets(),
+        ];
+    }
+
+    protected function menu(): array
+    {
+        return [
+            ...parent::menu(),
+        ];
+    }
+
+    /**
+     * @param ColorManager $colorManager
+     */
+    protected function colors(ColorManagerContract $colorManager): void
+    {
+        parent::colors($colorManager);
+
+        // $colorManager->primary('#00000');
+    }
+}
---
+++ app/Providers/MoonShineServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use MoonShine\Contracts\Core\DependencyInjection\CoreContract;
+use MoonShine\Laravel\DependencyInjection\MoonShine;
+use MoonShine\Laravel\DependencyInjection\MoonShineConfigurator;
+use App\MoonShine\Resources\MoonShineUser\MoonShineUserResource;
+use App\MoonShine\Resources\MoonShineUserRole\MoonShineUserRoleResource;
+
+class MoonShineServiceProvider extends ServiceProvider
+{
+    /**
+     * @param  CoreContract<MoonShineConfigurator>  $core
+     */
+    public function boot(CoreContract $core): void
+    {
+        $core
+            ->resources([
+                MoonShineUserResource::class,
+                MoonShineUserRoleResource::class,
+            ])
+            ->pages([
+                ...$core->getConfig()->getPages(),
+            ])
+        ;
+    }
+}
